c69c5ed8e0cf9f988cba08d2a7ea8b9186860872
Peter van Eck <peter.van.eck@inology.nl>
2016-06-30 18:11:15 +0200

    Add postgres pod create yaml file
    Authentication within the psql script in the docker
    image doesn't work yet.

--- postgres/docker/create-cyclosdb.sh
+++ postgres/docker/create-cyclosdb.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 <<-EOSQL
+psql -v ON_ERROR_STOP=1  --host stolon-proxy-service postgres --port 5432 -U stolon <<-EOSQL
+    password1
     CREATE ROLE cyclos WITH LOGIN SUPERUSER PASSWORD 'cyclos';
     CREATE DATABASE cyclos;
     GRANT ALL PRIVILEGES ON DATABASE cyclos TO cyclos;
